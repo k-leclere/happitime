aac936f453bfea23791e0da835b1d4bb4260ba6b
Kevin LECLERE <k.leclere@of2m.fr>
2021-11-23 16:53:03 +0100

    Ajout des services et villes + correction texte

--- components/Noel/Inscription.vue
+++ components/Noel/Inscription.vue
@@ -21,34 +21,116 @@
       <section>
       <label>Quelle est votre BU/Service ? <span>*</span></label>
       <select v-model="service" required>
-        <option>BU Immo</option>
-        <option>BU Auto</option>
-        <option>BU Emploi</option>
-        <option>BU Obsèques</option>
-        <option>BU Commerces et loisirs</option>
+        <option>Affiouest</option>
         <option>BU Alimentation</option>
+        <option>BU Auto</option>
+        <option>BU Commerce & Loisirs</option>
+        <option>BU Emploi</option>
         <option>BU Habitat</option>
-        <option>BU Services et Collectivités</option>
-        <option>BU Monétisation</option>
-        <option>Marketing transverse</option>
+        <option>BU Immo</option>
+        <option>BU Maville</option>
+        <option>BU Obsèques</option>
+        <option>BU Services & Collectivités</option>
+        <option>BU télévente</option>
+        <option>Caen</option>
+        <option>DAF</option>
+        <option>DEMO FM</option>
+        <option>Direction Générale</option>
+        <option>DRH</option>
         <option>DSI</option>
-        <option>DAF/RH</option>
-        <option>Studio</option>
-        <option>Medialex</option>
-        <option>RRR</option>
+        <option>Europe Régie Ouest</option>
+        <option>IST Lannion</option>
+        <option>Marketing transverse</option>
+        <option>MEDIALEX</option>
+        <option>Monétisation numérique</option>
+        <option>Océane Questember</option>
+        <option>Pole projets et méthodes</option>
+        <option>Precontact</option>
+        <option>Radio Hit West</option>
+        <option>Service informatique</option>
+        <option>Skillapp</option>
+        <option>Studio/ Pole technique</option>
+        <option>COCKTAIL FM</option>
       </select>
       </section>
       <section>
       <label>Quelle est votre agence ? <span>*</span></label>
       <select v-model="agence" required>
-        <option>Rennes (de noël ;-) )</option>
-        <option>Caen (le 17 decembre)</option>
-        <option>Nantes</option>
-        <option>Angers</option>
-        <option>Vannes</option>
+        <option>ALENCON</option>
+        <option>ANGERS</option>
+        <option>AUCH</option>
+        <option>BESANCON</option>
+        <option>BIARRITZ</option>
+        <option>BLOIS CEDEX</option>
+        <option>BOE</option>
+        <option>BORDEAUX</option>
+        <option>BRESSUIRE</option>
+        <option>BREST (rue Dupleix)</option>
+        <option>BREST (rue des Neireides)</option>
+        <option>BRIVE LA GAILLARDE</option>
+        <option>CABESTANY</option>
+        <option>CHAMPAGNE AU MONT D OR</option>
+        <option>CLERMONT-FERRAND</option>
+        <option>DIJON</option>
+        <option>DINAN</option>
+        <option>EPRON</option>
+        <option>EVREUX</option>
+        <option>FORBACH</option>
+        <option>LA GARDE</option>
+        <option>LA ROCHE SUR YON</option>
+        <option>LA ROCHELLE</option>
+        <option>LANGUEUX</option>
+        <option>LANNION (rue de Viarmes)</option>
+        <option>LANNION (rue St Marc)</option>
+        <option>LAVAL (rue de la paix)</option>
+        <option>LAVAL (av. R. Buron)</option>
+        <option>LE HAVRE</option>
+        <option>LE MANS</option>
+        <option>LILLE</option>
+        <option>LIMOGES</option>
+        <option>LISIEUX</option>
+        <option>LORIENT</option>
+        <option>LUCE</option>
+        <option>MARSEILLE</option>
+        <option>METZ</option>
+        <option>MONTAUBAN</option>
+        <option>MONTPELLIER</option>
+        <option>MULHOUSE</option>
+        <option>NANCY</option>
+        <option>NANTES</option>
+        <option>NICE</option>
+        <option>NIORT</option>
+        <option>PARIS 9</option>
+        <option>PAU</option>
+        <option>PLOERMEL</option>
+        <option>PONTIVY</option>
+        <option>QUESTEMBERT</option>
+        <option>QUIMPER (bd Dupleix)</option>
+        <option>QUIMPER (rue E. Perchec)</option>
+        <option>RENNES</option>
+        <option>ROUEN</option>
+        <option>SAINT GEORGES DES COTEAUX</option>
+        <option>SAINT JULIEN EN GENEVOIS</option>
+        <option>SAINT LEONARD</option>
+        <option>SAINT LO</option>
+        <option>SAINT OMER</option>
+        <option>SAINT QUENTIN</option>
+        <option>SAUMUR</option>
+        <option>ST HERBLAIN</option>
+        <option>ST MALO (av. J. Jaures)</option>
+        <option>ST MALO (bd des Talards)</option>
+        <option>ST NAZAIRE</option>
+        <option>STRASBOURG</option>
+        <option>TOULOUSE</option>
+        <option>TOURS</option>
+        <option>VALENCE</option>
+        <option>VANNES (6 bd de la Paix)</option>
+        <option>VANNES (4 bd de la Paix)</option>
+        <option>VITRE</option>
       </select>
       </section>
       <input type="submit" value="S'inscrire"/>
+    <h3 v-if="compteur>0">Déjà {{compteur}} inscrits.</h3>
 
     </form>
     <div v-else>
@@ -86,7 +168,16 @@ export default {
             prenom: '',
             service: '',
             agence: '',
+            compteur: 0,
         };
+    },
+    async mounted() {
+      const { data } = await this.$supabase
+        .rpc('nb_inscrits');
+
+        if(data) {
+          this.compteur = data;
+        }
     }
 };
 </script>
--- pages/noel/inscription.vue
+++ pages/noel/inscription.vue
@@ -7,19 +7,23 @@
                 …Enfin ceux qui veulent participer !!! ​
             </p>
             ​<p>
-                Cette année Happitime organise un arbre de Noël Additi.​<br/>
+                Cette année Happitime organise un arbre de Noël Additi *​<br/>
                 Serez-vous nombreux à vous prendre au jeu de la recherche des hobbies ​<br/>
                 et des goûts de vos collègues pour leur offrir un cadeau personnalisé qui leur fera plaisir ?​
             </p>
  ​           <p>
                     Règles du jeu : ​<br/>
-                1 – Inscription ci dessous avant le mardi 30 novembre 2021 ​<br/>
-                2 – Découvrez le nom de votre collègue à qui vous allez offrir un cadeau à partir du 1er décembre 2021​<br/>
-                3 - Trouvez LE cadeau personnalisé (budget maximum : 10€) ​<br/>
-                4 – Déposez vos cadeaux dans la hotte du père Noël avant le 16 décembre 2021 ​<br/>
-                5 – Rendez vous le vendredi 17 décembre à 10h00 dans le sapin​<br/>
+                1 – Inscription ci-dessous avant le mardi 30 novembre 2021 ​<br/>
+                2 – Découvrez le nom de votre collègue à qui vous allez offrir un cadeau le 1er décembre 2021 (tirage au sort**)<br/>
+                3 - Trouvez LE cadeau personnalisé (budget maximum : 10€) <br/>
+                4 – Déposez vos cadeaux dans la hotte du père Noël les 15 et 16 décembre 2021<br/>
+                5 – Rendez-vous le vendredi 17 décembre à 10h00 pour l'ouverture des cadeaux<br/>
             </p>
-
+            <em>
+                *Que vous soyez au siège ou en agence, vous êtes tous conviés.<br/>
+                **Tirage au sort organisé par agence ou en BU voisines au siège Rennais.<br/> 
+                Vous pourrez offrir un cadeau à un collègue de votre BU ou à un collègue de la BU juste à côté
+            </em>
         </div>
 
         <Inscription />
@@ -45,6 +49,10 @@ export default {
     background: #2a4a59;
     color: #fff;
     padding: 3rem 0;
+}
+
+em {
+    font-size: 0.7rem;
 }
 </style>
 
