dc3f0804ed8b9a480fa5cb962c877b1e9849c192
Kévin LECLERE <kevin.leclere@additi.fr>
2023-01-30 13:41:15 +0100

    test additi games

---
+++ components/Games/Inscription.vue
@@ -0,0 +1,114 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="inscription">
+    <form v-if="!isSuscribed" @submit.prevent="inscription()">
+      <section>
+        <label>Votre nom <span>*</span></label>
+        <input 
+          v-model="nom"
+          type="text"
+          required
+        />
+      </section>
+      <section>
+      <label>Votre prénom <span>*</span></label>
+      <input 
+        v-model="prenom"
+        type="text"
+        required
+      />
+      </section>
+      <section>
+        <label>Pouvez-vous apporter des jeux ?</label>
+        <input type="radio" name="jeux" value="1">Oui</input>
+        <input type="radio" name="jeux" value="0">Non</input>    
+      </section>
+      <section>
+      <label>Souhaitez-vous être Game Master ?</label>
+        <input type="radio" name="master" value="1">Oui</input>
+        <input type="radio" name="master" value="0">Non</input>
+      </section>
+      <input type="submit" value="S'inscrire"/>
+    <h3 v-if="compteur>0">Déjà {{compteur}} inscrits.</h3>
+
+    </form>
+    <div v-else>
+      Votre inscription a bien été prise en compte. Rendez-vous le 30 mars prochain !<br/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Inscription',
+    methods: {
+      async inscription() {
+        
+        const { data, error } = await this.$supabase
+          .from('noel')
+          .insert([
+            { nom: this.nom, prenom: this.prenom, jeux: this.jeux, master: this.master },
+          ]);
+        
+        if(!error) {
+          this.isSuscribed = true;
+        }
+        else {
+          console.log('ERREUR', error)
+        }
+      }
+    },
+    data() {
+        return {
+            isSuscribed: false,
+            nom: '',
+            prenom: '',
+            service: '',
+            agence: '',
+            compteur: 0,
+        };
+    },
+    async mounted() {
+      const { data } = await this.$supabase
+        .rpc('nb_inscrits');
+
+        if(data) {
+          this.compteur = data;
+        }
+    }
+};
+</script>
+
+
+<style scoped>
+.inscription {
+  padding: 3rem 0;
+}
+
+span {
+  color: #e2001a;
+}
+
+input,select {
+  display: block;
+  margin: 7px auto;
+  width: 320px;
+  padding: 5px 0;
+  text-align: center;
+}
+
+input[type=submit] {
+  font-size: 14px;
+  font-weight: 600;
+  height: 34px;
+  background: #e2001a;
+  color: #fff;
+  border: 0;
+  padding: 0 25px;
+  margin: 1.68em auto;
+  border-radius: 3px;
+  transition: all .4s;
+  width: 120px;
+}
+</style>
---
+++ pages/games/inscription.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="entete">
+            <h1>Inscription au 1er Additi Games</h1>
+            <p>
+                Le 30 mars 2023, Happitime organise les 1er Additi Games de 18h à 20h.
+            </p>
+            <p>
+                L'objectif est de s'amuser autours de jeux de société, 
+                plusieurs tables et game masters vous accueillerons afin de vous faire partager leur passion du jeu.
+            </p>
+            ​<p>
+                Attention les places sont limités à 73 participants. 
+            </p>
+        </div>
+
+        <inscription />
+    </div>
+</template>
+
+<script>
+
+import Inscription from '~/components/Games/Inscription.vue';
+
+export default {
+    name: 'PageInscription',
+    layout: 'default',
+    components: {
+        Inscription,
+    },
+};
+</script>
+
+
+<style scoped>
+.entete {
+    background: #2a4a59;
+    color: #fff;
+    padding: 3rem 0;
+}
+
+em {
+    font-size: 0.7rem;
+}
+</style>
+
