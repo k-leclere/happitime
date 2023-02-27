4a77d08ea15b74b328dd0be77498213d5c048edf
Kévin LECLERE <kevin.leclere@additi.fr>
2023-02-27 17:53:52 +0100

    Correction date

--- components/Games/Inscription.vue
+++ components/Games/Inscription.vue
@@ -42,7 +42,7 @@
 
     </form>
     <div v-else>
-      Votre inscription a bien été prise en compte. Rendez-vous le 30 mars prochain !<br/>
+      Votre inscription a bien été prise en compte. Rendez-vous le 6 avril prochain !<br/>
     </div>
   </div>
 </template>
--- pages/games/inscription.vue
+++ pages/games/inscription.vue
@@ -3,7 +3,7 @@
         <div class="entete">
             <h1>Inscription au 1er Additi Games</h1>
             <p>
-                Le 30 mars 2023, Happitime organise les 1er Additi Games de 18h à 20h.
+                Le 6 avril 2023, Happitime organise les 1er Additi Games de 18h à 20h.
             </p>
             <p>
                 L'objectif est de s'amuser autour de jeux de société, 
