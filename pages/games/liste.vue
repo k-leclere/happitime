44bbd48dd431060af55ef6417984ebd97064e3c8
Kévin LECLERE <kevin.leclere@additi.fr>
2023-03-08 18:04:10 +0100

    Add liste

---
+++ pages/games/liste.vue
@@ -0,0 +1,30 @@
+<template>
+    <liste/>
+</template>
+
+<script>
+
+import Liste from '~/components/Games/Liste.vue';
+
+export default {
+    name: 'GamesPageListe',
+    layout: 'default',
+    components: {
+        Liste,
+    },
+};
+</script>
+
+
+<style scoped>
+.entete {
+    background: #2a4a59;
+    color: #fff;
+    padding: 3rem 0;
+}
+
+em {
+    font-size: 0.7rem;
+}
+</style>
+
