6cdabe0a2fc760c4ff44e41863e453f8cc45db11
Kévin LECLERE <kevin.leclere@additi.fr>
2023-10-09 13:49:00 +0200

    Add halloween

---
+++ components/Halloween/Inscription.vue
@@ -0,0 +1,152 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="inscription">
+    <form v-if="!isSuscribed" @submit.prevent="inscription()">
+      <div v-if="isFull">
+        <b>L'événement est complet, mais inscrivez-vous !<br />
+        Nous vous contacterons en cas de désistement.</b>
+      </div>
+      
+      <section>
+        <label>Votre nom <span>*</span></label>
+        <input 
+          v-model="nom"
+          type="text"
+          required
+        />
+      </section>
+      <section>
+      <label>Votre prénom <span>*</span></label>
+      <input 
+        v-model="prenom"
+        type="text"
+        required
+      />
+      </section>
+      <section>
+        <label>Participer à la killer party ?</label>
+        <div class="radio">
+          <input type="radio" v-model="killer" name="killer" value="1" />Oui
+          <input type="radio" v-model="killer" name="killer" value="0" />Non
+        </div>
+      </section>
+      <section>
+        <label>Participer à la soirée ciné ?</label>
+        <div class="radio">
+          <input type="radio" v-model="film" name="film" value="1" />Oui
+          <input type="radio" v-model="film" name="film" value="0" />Non
+        </div>
+      </section>
+      <input type="submit" value="S'inscrire"/>
+    <h3 v-if="compteur>0">Déjà {{compteur}} inscrits.</h3>
+
+    </form>
+    <div v-else>
+      Votre inscription a bien été prise en compte.<br/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Inscription',
+    methods: {
+      async inscription() {
+        
+        this.setCompteur();
+
+        if(! this.isFull) {
+          const { data, error } = await this.$supabase
+            .from('games')
+            .insert([
+              { nom: this.nom, prenom: this.prenom, killer: this.killer, film: this.film },
+            ]);
+          
+          if(!error) {
+            this.isSuscribed = true;
+          }
+          else {
+            console.log('ERREUR', error)
+          }
+        }
+      },
+
+      async setCompteur() {
+        const { data } = await this.$supabase
+          .rpc('nb_inscrits_halloween');
+
+          if(data) {
+            this.compteur = data;
+          }
+        }
+    },
+    computed: {
+      isFull() {
+        return this.nbMax && this.compteur>=this.nbMax;
+      }
+    },
+    props: {
+        /**
+         * Détermine le nb max participants
+         */
+        nbMax: {
+            type: Number,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            isSuscribed: false,
+            nom: '',
+            prenom: '',
+            service: '',
+            agence: '',
+            compteur: 0,
+        };
+    },
+    async mounted() {
+      this.setCompteur();
+    }
+};
+</script>
+
+
+<style scoped>
+.inscription {
+  padding: 3rem 0;
+}
+
+span {
+  color: #e2001a;
+}
+
+input,select {
+  display: block;
+  margin: 7px auto;
+  width: 320px;
+  padding: 5px 0;
+  text-align: center;
+}
+
+input[type=submit] {
+  font-size: 14px;
+  font-weight: 600;
+  height: 34px;
+  background: #e2001a;
+  color: #fff;
+  border: 0;
+  padding: 0 25px;
+  margin: 1.68em auto;
+  border-radius: 3px;
+  transition: all .4s;
+  width: 120px;
+}
+.radio > * {
+  display: inline-block;
+  width: 30px;
+}
+section {
+  padding: 10px 0;
+}
+</style>
---
+++ pages/halloween/inscription.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="entete">
+            <h1>Inscription Halloween Party</h1>
+            ​<p>
+                Venez participer à nos événements Halloween !
+            </p>
+        </div>
+
+        <inscription :nb-max="nbMax"/>
+    </div>
+</template>
+
+<script>
+
+import Inscription from '~/components/Halloween/Inscription.vue';
+
+export default {
+    name: 'PageInscription',
+    layout: 'default',
+    components: {
+        Inscription,
+    },
+    data() {
+        return {
+            nbMax: 0,
+        };
+    },
+};
+</script>
+
+
+<style scoped>
+.entete {
+    background: #2a4a59;
+    color: #fff;
+    padding: 3rem 0;
+}
+
+em {
+    font-size: 0.7rem;
+}
+</style>
+
